a36677678bbf20c341ddeb535694920301ef27fd
Mei Sasaki <mesasaki@yahoo-corp.jp>
2017-06-10 15:28:47 +0900

    First commit.

---
+++ MotionSensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotionSensor
+//
+//  Created by 佐々木　明 on 2017/06/10.
+//  Copyright © 2017年 佐々木　明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
