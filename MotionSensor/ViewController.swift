b20971fc305058e386d3a09f66f90ca6371c13d8
Mei Sasaki <mesasaki@yahoo-corp.jp>
2017-06-10 19:21:16 +0900

    加速度センサーの数字を使えるように

--- MotionSensor/ViewController.swift
+++ MotionSensor/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    // MotionManager
+    let motionManager = CMMotionManager()
+    let guardThreshold = 0.9
+    let cutThreshold = 1.8
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        if motionManager.isAccelerometerAvailable {
+            // 加速度センサーの値取得間隔
+            motionManager.accelerometerUpdateInterval = 1.0
+            
+            // motionの取得を開始
+            motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { (data, error) in
+                // 取得した値をコンソールに表示
+                var flagCut = false
+                print("x: \(data?.acceleration.x) y: \(data?.acceleration.y) z: \(data?.acceleration.z)")
+                let accelNorm = sqrt(data!.acceleration.x * data!.acceleration.x + data!.acceleration.y * data!.acceleration.y + data!.acceleration.z * data!.acceleration.z)
+                if accelNorm > self.cutThreshold {
+                    print("Cut!")
+                    flagCut = true
+                }
+                
+                let accelXAbs = fabs(data!.acceleration.x)
+                if accelXAbs > self.guardThreshold && !flagCut {
+                    print("Guard!")
+                }
+        
+            }
+            )
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
